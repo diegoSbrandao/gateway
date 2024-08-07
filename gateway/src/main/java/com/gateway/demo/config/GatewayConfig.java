eca9843288ae4aeb1f93f850c16eba43b6e232b6
diegoSbrandao <diego.brandao@outlook.com>
2024-08-07 17:09:33 -0300

    adicionando novas lógicas

--- gateway/src/main/java/com/gateway/demo/config/GatewayConfig.java
+++ gateway/src/main/java/com/gateway/demo/config/GatewayConfig.java
@@ -5,27 +5,38 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class GatewayConfig {
-
-//    @Bean
-//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-//        return builder.routes()
-//                .route("client", r -> r
-//                        .path("/cep/**")
-//                        .uri("http://localhost:9000/"))
-////                .route("staff", r -> r
-////                        .path("/staff")
-////                        .uri("http://localhost:8083/"))
-////                .route("cep - Brasilapi", r -> r
-////                        .path("/cep/v1/**")
-////                        .filters(f -> f.rewritePath("/cep/v1/(?<cep>.*)", "/api/cep/v1/${cep}"))
-////                        .uri("https://brasilapi.com.br"))
-////                .route("cep - viacep", r -> r
-////                        .path("/cep/**")
-////                        .filters(f -> f.rewritePath("/cep/(?<cep>.*)", "/ws/${cep}/json/"))
-////                        .uri("https://viacep.com.br"))
-//                .build();
+//    @Configuration
+//    public class GatewayConf {
 //
+//        @Bean
+//        public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+//            return builder.routes()
+////                    .route("cep - Brasilapi", r -> r
+////                            .path("/cep/v1/**")
+////                            .filters(f -> f.rewritePath("/cep/v1/(?<cep>.*)", "/api/cep/v1/${cep}"))
+////                            .uri("https://brasilapi.com.br"))
+//                    .route("cep - viacep", r -> r
+//                            .path("/cep/v2/**")
+//                            .filters(f -> f.rewritePath("/cep/v2/(?<cep>.*)", "/ws/${cep}/json/"))
+//                            .uri("https://viacep.com.br"))
+//                    .route("client_route", r -> r
+//                            .path("/cep/v1/**")
+//                            .filters(f -> f
+//                                    .rewritePath("/cep/v1/(?<cep>.*)", "/cep/${cep}")
+//                                    .circuitBreaker(c -> c
+//                                            .setName("myCircuitBreaker")
+//                                            .setFallbackUri("forward:/fallback"))
+//                                    .filter((exchange, chain) -> {
+//                                        exchange.getRequest().mutate()
+//                                                .header("X-Fallback-Reason", "CircuitBreaker Open");
+//                                        return chain.filter(exchange);
+//                                    }))
+//                            .uri("http://localhost:9000/"))
+//                    .route("fallback_route", r -> r
+//                            .path("/fallback")
+//                            .filters(f -> f.rewritePath("/(?<cep>.*)", "/ws/${cep}/json/"))
+//                            .uri("https://viacep.com.br"))
+//                    .build();
+//
+//        }
 //    }
-}
